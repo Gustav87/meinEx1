1a71a6ed50c1c7d0fb946693bfee5d598dde15bb
Gustav Andersson <gustav.andersson@axfood.se>
2017-04-14 02:00:29 +0200

    tiny tunes completed

--- exercise/tiny-tunes/client/source/js/app.js
+++ exercise/tiny-tunes/client/source/js/app.js
@@ -1,1 +1,10 @@
-// your code here
+
+require('./x01')();
+require('./x02')();
+require('./x03')();
+require('./x04')();
+require('./x05')();
+require('./x06')();
+require('./x07')();
+require('./x08')();
+require('./x09')();
